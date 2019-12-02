92a21d8464d230bd43ff35895badbd6b65ddb4f7
joeljohnson02 <54966750+joeljohnson02@users.noreply.github.com>
2019-12-02 10:03:51 -0500

    WEbAPI and MErchant Class added to APis

---
+++ TermProjectTake2/PaymentAPI/PaymentAPI/Models/MerchantID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+	public class MerchantID
+	{
+		public String MerchantIDKey { get; set; }
+		
+	}
+}
---
+++ TermProjectTake2/PaymentAPI/PaymentAPI/Models/WebAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+	public class WebAPI
+	{
+		public String WebAPIKey { get; set; }
+	}
+}
